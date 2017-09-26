67703b7a4fe9b9513d58e3e7d8d785b8a6c54c60
livenerst <chambon.joffrey@neuf.fr>
2017-09-26 19:27:56 +0200

    fibo function

--- math_func.c
+++ math_func.c
@@ -13,7 +13,14 @@ unsigned long fact(unsigned long n) {
 }
 
 unsigned long fibo(unsigned long n) {
-    // FIX ME
+    if (n == 0) {
+        return 0;
+    }
+
+    else if (n == 1) {
+        return 1;
+    }
+    return fibo(n-1) + fibo (n-2);
 }
 
 unsigned long int_sqrt(unsigned long n) {
